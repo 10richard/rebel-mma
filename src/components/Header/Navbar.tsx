bf949d11ce942cf5d08c7a81cc5477a10a015abe
Richard Ten <rten04@icloud.com>
2023-10-17 23:56:24 -0400

    Make navbar and hero section responsive

---
+++ src/components/Header/Navbar.tsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import logo from "../../assets/rebelmma-logo.png";
+import closeMenu from "../../assets/close-menu-icon.svg";
+import hamburgerMenu from "../../assets/menu-icon.svg";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState<boolean>(false);
+  const [navbar, setNavbar] = useState<boolean>(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 80) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackground);
+
+  return (
+    <nav className="bg-black bg-transparent flex items-center justify-between px-10 py-2 z-50 w-full fixed">
+      <div>
+        <a href="/">
+          <img className="w-[200px]" src={logo} alt="RebelMMA Logo" />
+        </a>
+      </div>
+
+      <ul className="flex gap-10 text-white max-[800px]:hidden">
+        <li>
+          <a className="hover:text-seaserpent duration-300" href="/programs">
+            PROGRAMS
+          </a>
+        </li>
+        <li>
+          <a className="hover:text-seaserpent duration-300" href="/schedule">
+            SCHEDULE
+          </a>
+        </li>
+        <li>
+          <a className="hover:text-seaserpent duration-300" href="/about">
+            ABOUT
+          </a>
+        </li>
+        <li>
+          <a
+            className="bg-seaserpent px-3 py-2 text-black rounded-xl brightness-75 hover:brightness-100 duration-300"
+            href="/signup"
+          >
+            SIGN UP
+          </a>
+        </li>
+      </ul>
+
+      {/* Menu */}
+      <img
+        className={`min-[800px]:hidden w-8 cursor-pointer ${
+          toggle ? "hidden" : null
+        }`}
+        src={hamburgerMenu}
+        alt="Mobile menu"
+        onClick={() => setToggle((toggle) => !toggle)}
+      />
+
+      {/* Menu modal */}
+      <div
+        className={`fixed right-0 bottom-0 z-40 h-full w-[300px] min-[1150px]:hidden opacity-95 backdrop-blur-md bg-babyblue/80 text-lg ${
+          toggle ? "flex-col" : "hidden"
+        } `}
+      >
+        <div className="flex justify-end pr-10 pt-8 mb-10">
+          <img
+            className="w-8 cursor-pointer"
+            src={closeMenu}
+            alt="close menu"
+            onClick={() => {
+              setToggle((toggle) => !toggle);
+            }}
+          />
+        </div>
+
+        <ul className="text-white flex flex-col items-center gap-8 mb-16">
+          <li>
+            <a className="hover:text-seaserpent duration-300" href="/programs">
+              PROGRAMS
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-seaserpent duration-300" href="/schedule">
+              SCHEDULE
+            </a>
+          </li>
+          <li>
+            <a className="hover:text-seaserpent duration-300" href="/about">
+              ABOUT
+            </a>
+          </li>
+          <li>
+            <a
+              className="bg-seaserpent px-3 py-2 text-black rounded-xl brightness-75 hover:brightness-100 duration-300"
+              href="/signup"
+            >
+              SIGN UP
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,13 +5,14 @@ const Hero = () => {
   return (
     <section className="hero-section">
       <div className="hero-container">
-        <div className="hero-text absolute top-1/3 right-1/2 z-10 w-[550px] text-white">
-          <h1 className="font-bold text-6xl mb-5">
+        <div className="hero-text absolute z-10 top-1/3 left-[10%] text-white">
+          <h1 className="font-bold text-4xl min-[650px]:text-6xl mb-5 min-[650px]:w-[550px] w-[315px]">
             Rebel Against Ordinary Fitness
           </h1>
+          {/* Add a breakpoint similar to Strategic Academy */}
           <p className="mb-10">Join the #1 martial arts gym in NJ</p>
           <a
-            className="bg-seaserpent text-xl text-black p-6 py-3 rounded-xl brightness-75 hover:brightness-100 duration-300"
+            className="bg-seaserpent text-lg text-black px-5 py-2.5 rounded-xl brightness-75 hover:brightness-100 duration-300"
             href="/signup"
           >
             JOIN NOW
