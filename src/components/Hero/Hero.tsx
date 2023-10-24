add535813588dde6d67d5c958c9499d002b5c003
Richard Ten <rten04@icloud.com>
2023-10-24 17:46:29 -0400

    Modify JOIN NOW link

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import HeroImg from "../../assets/hero.png";
 
 const Hero = () => {
@@ -12,7 +11,7 @@ const Hero = () => {
           {/* Add a breakpoint similar to Strategic Academy */}
           <p className="mb-10">Join the #1 martial arts gym in NJ</p>
           <a
-            className="bg-seaserpent text-lg text-black px-5 py-2.5 rounded-xl brightness-75 hover:brightness-100 duration-300"
+            className="bg-seaserpent text-lg text-black px-5 py-2.5 rounded-xl brightness-[0.8] hover:brightness-100 duration-300"
             href="/signup"
           >
             JOIN NOW
