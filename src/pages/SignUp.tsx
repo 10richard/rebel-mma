677f9eb606a1c83f8dd8cda3a1f9688483c35d6f
Richard Ten <rten04@icloud.com>
2023-10-30 19:54:45 -0400

    Finish SignUp page and add GoogleMaps component

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,14 +1,22 @@
 import Banner from "../components/Banner/Banner";
+import GoogleMaps from "../components/GoogleMaps/GoogleMaps";
 import SignUpBox from "../components/SignUpBox/SignUpBox";
 
 const SignUp = () => {
   return (
     <>
       <Banner heading="Sign Up" />
-      <SignUpBox
-        headingOne="FREE TRIAL SIGN UP"
-        headingTwo="TAKE THE FIRST STEP TODAY"
-      />
+      <div className="grid min-[1200px]:grid-cols-2 items-center">
+        <SignUpBox headingOne="FREE TRIAL" headingTwo="TAKE THE FIRST STEP" />
+        <GoogleMaps />
+        {/* <div className="min-[1200px]:h-[450px] px-10">
+          <img
+            className="object-cover h-full rounded-xl border-2 border-primary shadow-lg"
+            src={runningImg}
+            alt="Person running on treadmill"
+          />
+        </div> */}
+      </div>
     </>
   );
 };
